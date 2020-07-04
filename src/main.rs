3f0088c151e5995cbfb4ed18675af6cf3a1bcdc2
Turtwiggy <m-p-b@hotmail.co.uk>
2020-07-04 15:41:31 +0100

    - rendering imgui gui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate gl;
+extern crate imgui;
+extern crate imgui_opengl_renderer;
+extern crate imgui_sdl2;
 extern crate sdl2;
 
 pub mod shader;
+pub mod game_window;
 
 use gl::types::GLint;
 use std::ffi::CString;
@@ -9,122 +13,142 @@ use std::time::Instant;
 
 // settings
 const SCR_WIDTH: u32 = 720;
-const SCR_WIDTH_GL: GLint = SCR_WIDTH as GLint;
 const SCR_HEIGHT: u32 = 480;
-const SCR_HEIGHT_GL: GLint = SCR_HEIGHT as GLint;
 
 fn main() {
     //Window Init
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+    {
+        let gl_attr = video.gl_attr();
+        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+        gl_attr.set_context_version(3, 3);
+    }
 
-    let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(3, 1);
-
-    let window = video_subsystem
-        .window("Game", SCR_WIDTH, SCR_HEIGHT)
-        .opengl()
+    let window = video
+        .window("rust-imgui-sdl2 demo", SCR_WIDTH, SCR_HEIGHT)
+        .position_centered()
         .resizable()
+        .opengl()
+        .allow_highdpi()
         .build()
         .unwrap();
 
     //OpenGL
-    let _gl_context = window.gl_create_context().unwrap();
-    let _gl =
-        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let _gl_context = window
+        .gl_create_context()
+        .expect("Couldn't create GL context");
+    gl::load_with(|s| video.gl_get_proc_address(s) as _);
 
+    //ImGui + SDL2
+    let mut imgui = imgui::Context::create();
+    imgui.set_ini_filename(None);
+    let mut imgui_sdl2 = imgui_sdl2::ImguiSdl2::new(&mut imgui, &window);
+    let renderer =
+        imgui_opengl_renderer::Renderer::new(&mut imgui, |s| video.gl_get_proc_address(s) as _);
+
+    /*
     //A Shader
-    let vert_shader = shader::Shader::from_vert_source(
-        &CString::new(include_str!("data/shaders/triangle.vert")).unwrap(),
-    )
-    .unwrap();
-    let frag_shader = shader::Shader::from_frag_source(
-        &CString::new(include_str!("data/shaders/triangle.frag")).unwrap(),
-    )
-    .unwrap();
-    let shader_program = shader::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
-    shader_program.set_used();
-
+    // let vert_shader = shader::Shader::from_vert_source(
+    //     &CString::new(include_str!("data/shaders/triangle.vert")).unwrap(),
+    // ).unwrap();
+    // let frag_shader = shader::Shader::from_frag_source(
+    //     &CString::new(include_str!("data/shaders/triangle.frag")).unwrap(),
+    // ).unwrap();
+    // let shader_program = shader::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
+    // shader_program.set_used();
     //A triangle
-    let vertices: Vec<f32> = vec![
-        // positions      // colors
-        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
-        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
-        0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
-    ];
+    // let vertices: Vec<f32> = vec![
+    //     // positions      // colors
+    //     0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
+    //     -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
+    //     0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
+    // ];
+    // //VBO
+    // let mut vbo: gl::types::GLuint = 0;
+    // unsafe {
+    //     gl::GenBuffers(1, &mut vbo);
+    // }
+    // unsafe {
+    //     gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    //     gl::BufferData(
+    //         gl::ARRAY_BUFFER,                                                       // target
+    //         (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+    //         vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
+    //         gl::STATIC_DRAW,                               // usage
+    //     );
+    //     gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    // }
+    // //VAO
+    // let mut vao: gl::types::GLuint = 0;
+    // unsafe {
+    //     gl::GenVertexArrays(1, &mut vao);
+    // }
+    // unsafe {
+    //     gl::BindVertexArray(vao);
+    //     gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    //     gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+    //     gl::VertexAttribPointer(
+    //         0,         // index of the generic vertex attribute ("layout (location = 0)")
+    //         3,         // the number of components per generic vertex attribute
+    //         gl::FLOAT, // data type
+    //         gl::FALSE, // normalized (int-to-float conversion)
+    //         (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+    //         std::ptr::null(),                                     // offset of the first component
+    //     );
+    //     gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
+    //     gl::VertexAttribPointer(
+    //         1,         // index of the generic vertex attribute ("layout (location = 0)")
+    //         3,         // the number of components per generic vertex attribute
+    //         gl::FLOAT, // data type
+    //         gl::FALSE, // normalized (int-to-float conversion)
+    //         (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+    //         (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
+    //     );
+    //     gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    //     gl::BindVertexArray(0);
+    //     gl::Viewport(0, 0, SCR_WIDTH as GLint, SCR_HEIGHT as GLint); // set viewport
+    // }
 
-    //VBO
-    let mut vbo: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut vbo);
-    }
-    unsafe {
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,                                                       // target
-            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
-            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
-            gl::STATIC_DRAW,                               // usage
-        );
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-    }
-    //VAO
-    let mut vao: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-    }
-    unsafe {
-        gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+    */
 
-        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            0,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            std::ptr::null(),                                     // offset of the first component
-        );
-        gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            1,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
-        );
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut last_frame = Instant::now();
 
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
-    }
+    'running: loop {
+        use sdl2::event::Event;
+        use sdl2::keyboard::Keycode;
+        for event in event_pump.poll_iter() {
+            imgui_sdl2.handle_event(&mut imgui, &event);
+            if imgui_sdl2.ignore_event(&event) {
+                continue;
+            }
 
-    //Viewport
-    unsafe {
-        gl::Viewport(0, 0, SCR_WIDTH_GL, SCR_HEIGHT_GL); // set viewport
-    }
-
-    let mut last_frame: Instant = Instant::now();
-    let mut event_pump = sdl.event_pump().unwrap();
-    'main: loop {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        imgui_sdl2.prepare_frame(imgui.io_mut(), &window, &event_pump.mouse_state());
         let now = Instant::now();
         let delta = now - last_frame;
         let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
         last_frame = now;
-
-        for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit { .. } => break 'main,
-                _ => {}
-            }
+        imgui.io_mut().delta_time = delta_s;
+        let ui = imgui.frame();
+        ui.show_demo_window(&mut true);
+        unsafe {
+            gl::ClearColor(0.2, 0.2, 0.2, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
-
-        shader_program.set_used();
-        render(vao);
-
+        imgui_sdl2.prepare_render(&ui, &window);
+        renderer.render(ui);
         window.gl_swap_window();
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
